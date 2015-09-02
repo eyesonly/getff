712c66f04c472124b3ba9547c4ce6ca8d99311bf
Jonathan Groll <lists@groll.co.za>
2015-09-02 15:20:16 +0200

    applied fix for a change on RFI web site

--- getff.py
+++ getff.py
@@ -3,17 +3,18 @@ from  urllib2 import urlopen
 from re       import search,compile
 
 RFIURL  = 'http://telechargement.rfi.fr.edgesuite.net/rfi/francais/audio/journaux/derniers/journal_francais_facile.mp3.asx'
-PREFIX  = 'http://telechargement.rfi.fr.edgesuite.net/rfi/francais/audio/journaux/r001/'
-RE      = compile(r'r001/(.+?).wsx')
+PREFIX  = 'http://telechargement.rfi.fr.edgesuite.net/rfi/francais/audio/jff/'
+RE      = compile(r'jff/(.+?)/(.+?).wsx')
 
 # 'fetchme' is the URL for today's actualite
 asx     = urlopen(RFIURL).read()
-mp3     = search(RE, asx).group(1)
-fetchme = PREFIX + mp3
+yearmo  = search(RE, asx).group(1)
+mp3     = search(RE, asx).group(2)
+fetchme = PREFIX + yearmo + '/' + mp3
 
-# ...and download it
+# ...and download it - save as a file called 'mp3'
 print fetchme
-req     = urlopen(fetchme)
+req = urlopen(fetchme)
 fp  = open(mp3, 'wb')     
 for line in req:          
     fp.write(line)        
